d51a9bf64812546a38cb56bdd7180696f3eb6302
jiwon <wldnjs9515@naver.com>
2023-12-13 21:25:03 +0900

    Setting: 백엔드 셋팅

---
+++ newMFC/src/main/java/com/ssafy/newMFC/MewMfcApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.newMFC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MewMfcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MewMfcApplication.class, args);
+	}
+
+}
---
+++ newMFC/src/test/java/com/ssafy/newMFC/MewMfcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.newMFC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MewMfcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
